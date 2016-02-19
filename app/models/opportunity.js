191a6a2cb321b307b662b51098a4f45e499f5eb2
Kevin Mircovich <kmircovich1@gmail.com>
2016-02-19 16:38:57 -0500

    opportunities showing up

--- app/models/opportunity.js
+++ app/models/opportunity.js
@@ -3,8 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   position: DS.attr('string'),
-  description: DS.attr('string'),
-  user: DS.belongsTo('user'),
-  contacts: DS.hasMany('contact'),
-  notes: DS.hasMany('note')
+  description: DS.attr('string')
 });
--- app/router.js
+++ app/router.js
@@ -7,9 +7,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('users');
-  this.route('opportunities', function() {
-    this.route('opportunity', { path: '/opportunities/:opportunity_id' });
-  });
+  this.route('opportunities', function() {});
   this.route('login');
   this.route('signup');
 });
