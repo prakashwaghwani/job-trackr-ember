18b21536a7af6d00f5634c75fd8631e7e13b916f
Kevin Mircovich <kmircovich1@gmail.com>
2016-02-22 10:47:27 -0500

    mid way through opp page

--- app/routes/opportunity.js
+++ app/routes/opportunity.js
@@ -3,5 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params){
     return this.store.findRecord('opportunity', params.opportunity_id);
+  },
+  actions: {
+    save(){
+      let opportunity = this.currentModel
+      const newNote = this.get('store').createRecord('note', this.modelFor('note'));
+      newNote.set('opportunity', opportunity);
+      newNote.save();
+    }
   }
 });
