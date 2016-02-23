915ba2865b2aea885acecaeac97f969af3f6f95a
Kevin Mircovich <kmircovich1@gmail.com>
2016-02-22 20:48:23 -0500

    added dates

--- app/models/note.js
+++ app/models/note.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   content: DS.attr('string'),
   attachment: DS.attr('file'),
+  created_at: DS.attr('date'),
   opportunity: DS.belongsTo('opportunity')
 });
