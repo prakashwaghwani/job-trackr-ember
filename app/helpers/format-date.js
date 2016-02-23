5fdc360e40f5801efca011297c2f009008baf179
Matteo  Ziff <matteoziff@Matteos-MacBook-Pro.local>
2016-02-23 10:48:56 -0500

    added date and time

--- app/helpers/format-date.js
+++ app/helpers/format-date.js
@@ -2,8 +2,8 @@ import Ember from 'ember';
 
 export function formatDate(datetime) {
   var datetime = new Date(datetime);
-  var date = datetime.toDateString() + " " + datetime.toLocaleTimeString()
-  return date
+  var date = datetime.toDateString() + " " + datetime.toLocaleTimeString();
+  return date;
 }
 
 export default Ember.Helper.helper(formatDate);
