147b56b54a5a9aa40d372a4e4ffb6521b6e3190e
Kevin Mircovich <kmircovich1@gmail.com>
2016-02-24 10:55:20 -0500

    changed host to production api

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,7 +3,7 @@ import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 
 export default ActiveModelAdapter.extend(DataAdapterMixin, {
-  host: 'http://localhost:3000',
+  host: 'https://job-tracker-api.herokuapp.com/',
   namespace: 'api/v1',
   authorizer: 'authorizer:devise'
 });
